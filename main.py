b938a99659feabfe7676925f4dcc64660b42574c
zarana savani <129183177+zaranasavani@users.noreply.github.com>
2024-02-04 22:54:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import qrcode as qr
 img = qr.make("https://www.youtube.com/@milansavani3742")   #give the link where you want to go from QR-code
-img.save("milan_youtube.png")        #generate thayelo qr code ne je save karavo hoy te file type
+img.save("milan_youtube.png")        #file type for saving generated QR-code
